db220bfc35070bd686996c4e9e1f349c0249a550
Henrik Santos <henrik.ruan4@gmail.com>
2023-01-11 23:50:41 -0300

    test: altera a descrição do teste

--- test/unit/hadleTalkers.test.js
+++ test/unit/hadleTalkers.test.js
@@ -9,8 +9,9 @@ const {
 } = require('../../src/utils/handleTalkers');
 
 describe('testa as funções relacionadas a rota talker', function () {
-  it('testa a função getTalker', async function () {
+  it('testa se a função getTalker retorna um array', async function () {
     const talkers = await getTalkers();
+    
     expect(talkers).to.be.instanceOf(Array);
   })
 })
